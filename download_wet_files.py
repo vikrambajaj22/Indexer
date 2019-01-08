aa382dcc77b4ad74cc05dd3803892e0a9fcb4be4
Vikram Bajaj <vikrambajaj@nyu.edu>
2019-01-08 12:13:54 -0500

    Add files via upload

---
+++ download_wet_files.py
@@ -0,0 +1,36 @@
+# Vikram Sunil Bajaj (vsb259)
+
+# First, download the WET files file list (CC-MAIN-2018-39/wet.paths.gz) from http://commoncrawl.org/2018/10/september-2018-crawl-archive-now-available/ and extract it.
+# Then, run this code.
+
+# import packages
+import os
+import requests
+
+
+# create a directory to store the downloaded files
+if not os.path.exists('wet_files'):
+    os.mkdir('wet_files')
+
+
+# open the extracted file list
+with open('wet.paths/wet.paths', 'r') as f:
+    file_paths = f.readlines()
+
+
+# get the first 50 (feel free to customize)
+first_50 = ['https://commoncrawl.s3.amazonaws.com/' + path.strip('\n') for path in file_paths[:50]]
+
+
+# download the files and store them in the wet_files directory
+i = 0
+for path in first_50:
+    file_name = 'wet_files/' + path[path.rindex('-')+1:]
+    if not os.path.exists(file_name):
+        print('Downloading File %d ...' % (i+1))
+        res = requests.get(path)
+        with open(file_name, 'wb') as f:
+            f.write(res.content)
+    else:
+        print(file_name, 'already exists! Skipping File %d' % (i+1))
+    i += 1
